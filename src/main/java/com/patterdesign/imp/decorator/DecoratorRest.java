6e89c0fa49bb13789402517a4295bf8c3413b475
SilentKush <osvaldsilent@gmail.com>
2022-05-25 02:23:49 -0500

    decorator redone and added

--- src/main/java/com/patterdesign/imp/decorator/DecoratorRest.java
+++ src/main/java/com/patterdesign/imp/decorator/DecoratorRest.java
@@ -1,4 +1,29 @@
 package com.patterdesign.imp.decorator;
 
-public class DecoratorRest {
+import com.patterdesign.interfa.AbstractDecorator;
+import com.patterdesign.interfa.IServiceRest;
+
+public class DecoratorRest extends AbstractDecorator {
+    private static DecoratorRest decoratorRest;
+    private static IServiceRest iServiceRest;
+    private DecoratorRest(IServiceRest iServiceRest){
+        super(iServiceRest);
+        this.iServiceRest=iServiceRest;
+    }
+
+    public static DecoratorRest getInstance(IServiceRest serviceRest){
+        if(decoratorRest==null) decoratorRest = new DecoratorRest(serviceRest);
+        return decoratorRest;
+    }
+
+    @Override
+    public void parse() {
+        System.out.println("parsing data before call");
+        callService();
+    }
+
+    @Override
+    public void callService() {
+        iServiceRest.callService();
+    }
 }
